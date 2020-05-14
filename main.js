4ebf1ce38e46f9c09815a350f3e8a53f3105bfc1
Łukasz Frągnowski <lukasz.fragnowski@gmail.com>
2020-05-14 22:13:29 +0200

    format all files with prettier extension\

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const firstName = 'Lukasz';
+const firstName = "Lukasz";
 const age = 37;
-
 
 console.log(firstName);
 console.log(age);
 
 console.log(`cześć, nazywam się ${firstName} i mam ${age} lat.`);
- 
