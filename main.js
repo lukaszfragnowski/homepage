9d21bfcf61eb2c5bdb2cef35278574699b525219
Łukasz Frągnowski <lukasz.fragnowski@gmail.com>
2020-05-17 14:39:26 +0200

    new BOM classes, updated styles and added DOM modifications in JS

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ console.log(firstName);
 console.log(age);
 
 console.log(`cześć, nazywam się ${firstName} i mam ${age} lat.`);
+
+
+const heading = document.querySelector('.company-name--js');
+
+heading.innerHTML = `cześć, nazywam się ${firstName} i mam ${age} lat.`;
