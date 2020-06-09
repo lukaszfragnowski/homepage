cb3dca509c33386bb08c8cc8e6c3d08146b020a9
Łukasz Frągnowski <lukasz.fragnowski@gmail.com>
2020-06-09 11:37:11 +0200

    hamburger menu script moved to another file

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// hamburger menu
-
-const hamburger = document.querySelector('.hamburger--js');
-
-hamburger.addEventListener('click', () => {
-  const nav = document.querySelector('.navigation--js');
-  nav.classList.toggle('navigation--open');
-});
-
 
 
 
---
+++ menu.js
@@ -0,0 +1,8 @@
+// hamburger menu
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+  const nav = document.querySelector('.navigation--js');
+  nav.classList.toggle('navigation--open');
+});
